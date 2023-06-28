6a5bfba554b088a24f883be6c0db0fb300d937a4
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-06-28 12:09:09 -0700

    fixing spelling errors

--- haydenlenca-react-portfolio/src/components/pages/About.js
+++ haydenlenca-react-portfolio/src/components/pages/About.js
@@ -4,9 +4,9 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <p>Hello everyone, in this section I will tell you a little about me. I grew up in Michigan and moved to Arizona to persue a better life path.
-      after spending a few years saving money I decided to get into web development. After trying on my own it became very frusterating.
-      On the verg of giving up I decided to take a bootcamp from MSU to get me on the path to greatness. Now here I am building web pages and persuing the path that will be a great acheivment.
+      <p>Hello everyone, in this section I will tell you a little about me. I grew up in Michigan and moved to Arizona to pursue a better life path.
+      after spending a few years saving money I decided to get into web development. After trying on my own it became very frustrating.
+      On the verge of giving up I decided to take a bootcamp from MSU to get me on the path to greatness. Now here I am building web pages and pursuing the path that will be a great achievement.
       </p>
     </div>
   );
--- haydenlenca-react-portfolio/src/components/pages/Resume.js
+++ haydenlenca-react-portfolio/src/components/pages/Resume.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Blog() {
   return (
     <div>
-      <h1>Blog Page</h1>
+      <h1>Resume Page</h1>
       <p>
-        i will change this to extra stuff i work on 
+        i will 
       </p>
     </div>
   );
