9ff20b202698e6d4e63234323d0d2ab908430525
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-06-29 10:19:12 -0700

    working on resume page

--- haydenlenca-react-portfolio/src/components/pages/Resume.js
+++ haydenlenca-react-portfolio/src/components/pages/Resume.js
@@ -1,30 +1,36 @@
 import React from 'react';
 
+// const styles = {
+//   liStyle{
+    
+//   },
+// };
+
 export default function Blog() {
   return (
     <div>
       <h1>Resume Page</h1>
       
       <div className="resume-download">
-              <a href="https://1drv.ms/w/s!AoAqX8Ddqs3QvDpyhfEUlkvpuWlH?e=laWjbU" download>Download My Resume</a>
+              <a href="https://1drv.ms/w/s!AoAqX8Ddqs3QvDpyhfEUlkvpuWlH?e=laWjbU" download>View and Download My Resume</a>
             </div>
 
             <h4>Proficiencies</h4>
-              <ul className="list-inline">
-                <li className="list-inline-item">HTML/CSS/Bootstrap</li>
-                <li className="list-inline-item">JavaScript/jQuery</li>
-                <li className="list-inline-item">Node.js</li>
-                <li className="list-inline-item">React</li>
-                <li className="list-inline-item">MYSQL</li>
-                <li className="list-inline-item">NoSQL</li>
-                <li className="list-inline-item">Model View Controller</li>
-                <li className="list-inline-item">Progressive Web Applications</li>
-                <li className="list-inline-item">Git</li>
-                <li className="list-inline-item">Node.js</li>
-                <li className="list-inline-item">Express.js</li>
-                <li className="list-inline-item">Object Oriented Programming</li>
-                <li className="list-inline-item">REST APIs</li>
-                <li className="list-inline-item">JSON</li>
+              <ul>
+                <li>HTML/CSS/Bootstrap</li>
+                <li>JavaScript/jQuery</li>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>MYSQL</li>
+                <li>NoSQL</li>
+                <li>Model View Controller</li>
+                <li>Progressive Web Applications</li>
+                <li>Git</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>Object Oriented Programming</li>
+                <li>REST APIs</li>
+                <li>JSON</li>
               </ul>
     </div>
   );
